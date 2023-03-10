fb39a3dcb4360a6023b2bb41b6d44556d5b76468
Katy Rosli <katyrosli@gmail.com>
2023-03-10 07:16:33 +0100

    added rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Map from './components/Map';
-import CityList from './components/CityList';
+import Rules from './components/Rules';
 
 const App = () => {
+  const [showMap, setShowMap] = useState(false);
+  const [showRules, setShowRules] = useState(true);
+
+  // Use effect to retrieve showRules value from local storage
+  useEffect(() => {
+    const showRulesFromStorage = localStorage.getItem('showRules');
+    setShowRules(showRulesFromStorage ? JSON.parse(showRulesFromStorage) : true);
+
+    const showMapFromStorage = localStorage.getItem('showMap');
+    setShowMap(showMapFromStorage ? JSON.parse(showMapFromStorage) : false);
+}, []);
+
+  // Toggle showMap and showRules states and update local storage value
+  const handleShowMap = () => {
+    setShowMap(true);
+    setShowRules(false);
+    localStorage.setItem('showRules', JSON.stringify(false));
+    localStorage.setItem('showMap', JSON.stringify(true));
+  }
+
   return (
-    <>
-      <Map />
-    </>
+    <div>
+          {/* Render Rules component only if showRules is true */}
+          {showRules && (
+            <>
+              <Rules onShowMap={() => handleShowMap()} />
+              <button onClick={handleShowMap}>Ready!</button>
+              </>
+          )}
+          {/* Render Map component only if showMap is true */}
+          {showMap && <Map />}
+        </div>
   );
 };
 
